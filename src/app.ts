246e03394f89e187e1e6ebd2e09f47823b16bbb0
migromarj <miguelromeroarjona@gmail.com>
2024-02-20 11:31:43 +0100

    Initial project setup

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
+import metamorphicTesting from './routes/metamorphicTesting'
+import './loadEnv'
+
+const app: Express = express()
+const API_VERSION = '/api/v1'
+
+app.use(express.json())
+app.use(cors())
+
+app.get(API_VERSION, (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
+
+app.use(API_VERSION + '/metamorphicTesting', metamorphicTesting)
+
+module.exports = app
