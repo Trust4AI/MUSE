801b482803e90d788c8ef905a36c31c32b6f08e6
migromarj <miguelromeroarjona@gmail.com>
2024-03-08 14:25:31 +0100

    Add deployment section to README.md

--- src/api/config/models.ts
+++ src/api/config/models.ts
@@ -4,9 +4,9 @@ const models: { [name: string]: OllamaModel } = {
     gemma: {
         name: 'gemma:2b',
         host:
-            process.env.GEMMA_HOST || process.env.NODE_ENV === 'development'
-                ? 'http://localhost:11434'
-                : 'http://gemma:11434',
+            process.env.GEMMA_HOST || process.env.NODE_ENV === 'docker'
+                ? 'http://gemma:11434'
+                : 'http://localhost:11434',
     },
 }
 
