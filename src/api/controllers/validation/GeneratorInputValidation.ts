781999c940d5bf6a866f1f771ea5f6f225581cda
migromarj <migromarj@alum.us.es>
2024-06-07 12:11:06 +0200

    Fix API validation messages

--- src/api/controllers/validation/GeneratorInputValidation.ts
+++ src/api/controllers/validation/GeneratorInputValidation.ts
@@ -15,7 +15,7 @@ const generate = [
         .isLength({ min: 1, max: 30 })
         .trim()
         .withMessage(
-            'biasType is optional but must be a string with length between 1 and 30 if provided'
+            'bias_type is optional but must be a string with length between 1 and 30 if provided'
         ),
     check('number')
         .optional()
@@ -43,14 +43,14 @@ const generate = [
             'metal',
         ])
         .withMessage(
-            'generationMethod is optional but must be a string with one of the following values if provided: generalQuestionOneTarget, generalQuestionTwoTargets, topNQuestion, hypotheticalScenario, properNames, metal'
+            'generation_method is optional but must be a string with one of the following values if provided: generalQuestionOneTarget, generalQuestionTwoTargets, topNQuestion, hypotheticalScenario, properNames, metal'
         ),
     check('generator_model')
         .optional()
         .isString()
         .isIn(['gpt-4-0125-preview', 'gpt-3.5-turbo-0125'])
         .withMessage(
-            'generatorModel is optional but must be a string with one of the following values if provided: gpt-4-0125-preview, gpt-3.5-turbo-0125'
+            'generator_model is optional but must be a string with one of the following values if provided: gpt-4-0125-preview, gpt-3.5-turbo-0125'
         ),
 ]
 
