3e0dba33259925482a9ba48c33d4299f0ff7c3d3
migromarj <miguelromeroarjona@gmail.com>
2025-01-21 16:06:02 +0100

    Change Gemini topK parameter

--- src/api/services/GeminiGenerationModelService.ts
+++ src/api/services/GeminiGenerationModelService.ts
@@ -28,7 +28,7 @@ class GeminiGenerationModelService {
         const generationConfig: GeminiGenerationConfig = {
             temperature: generatorTemperature,
             topP: 0.95,
-            topK: 64,
+            topK: 40,
             maxOutputTokens: 8192,
             response_mime_type: 'text/plain',
         }
