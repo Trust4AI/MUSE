75249a5db44d12ad4e5e5a68ee73d451db9c5be7
Miguel Romero Arjona <migromarj@alum.us.es>
2025-04-28 09:00:22 -0400

    Update input validation

--- src/api/controllers/validation/GeneratorInputValidation.ts
+++ src/api/controllers/validation/GeneratorInputValidation.ts
@@ -12,7 +12,7 @@ const generate = [
         .trim()
         .custom((value: string): boolean => {
             const generatorModels: string[] = getGeneratorModelsList()
-            if (value && !generatorModels.includes(value)) {
+            if (value === '' || !generatorModels.includes(value)) {
                 throw new Error(
                     `generator_model must be a string, if provided, with one of the following values: [${generatorModels.join(
                         ', '
@@ -27,7 +27,7 @@ const generate = [
         .trim()
         .custom((value: string): boolean => {
             const generationMethods: string[] = getGenerationMethods()
-            if (value && !generationMethods.includes(value)) {
+            if (value === '' || !generationMethods.includes(value)) {
                 throw new Error(
                     `generation_method is optional but must be a string with one of the following values if provided: [${generationMethods.join(
                         ', '
